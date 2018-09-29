09b1c11ab1c6df7cabd36636d09ec4ebb7281bef
Alex <grashupfer99@gmail.com>
2018-09-29 18:00:41 +0900

    unidir-data-flow

--- src/App.js
+++ src/App.js
@@ -29,43 +29,59 @@ class App extends Component {
     this.state = {
       list: list
     };
+
+    this.onDismiss = this.onDismiss.bind(this);
   }
 
-  userService() {}
+  onDismiss(id) {
+    // 1st step
+    // const updatedList = this.state.list.filter( function isNotId(item){
+    //   return item.objectID !== id
+    // })
+
+    // 2nd step
+    // function isNotId(item) {
+    //   return item.objectID !== id;
+    // }
+
+    // const updatedList = this.state.list.filter(isNotId);
+
+    // 3rd step
+    // const isNotId = item => item.objectID !== id;
+    // const updatedList = this.state.list.filter(isNotId);
+
+    // 4th step (refactored)
+    const updatedList = this.state.list.filter(item => item.objectID !== id);
+    this.setState({ list: updatedList });
+  }
 
   render() {
     return (
       <div className="App my-4">
         {this.state.list.map(item => (
           <div
-            className="w-50 mx-auto my-2 bg-primary border rounded shadow-lg"
             key={item.objectID}
+            className="bg-info rounded m-2 d-flex justify-content-between w-75 mx-auto"
           >
-            <span className="d-block">
-              <a className="font-weight-bold text-dark" href={item.url}>
+            <span className="my-auto mx-2">
+              <a className="text-dark font-weight-bold" href={item.url}>
                 {item.title}
               </a>
             </span>
-            <span className="d-block">{item.author}</span>
-            <span className="d-block">{item.num_comments}</span>
-            <span className="d-block">{item.points}</span>
+            <span className="my-auto">{item.author}</span>
+            <span className="my-auto">{item.num_comments}</span>
+            <span className="my-auto">{item.points}</span>
+            <span className="my-auto">
+              <button
+                className="btn btn-danger m-2"
+                onClick={() => this.onDismiss(item.objectID)}
+                type="button"
+              >
+                Dismiss
+              </button>
+            </span>
           </div>
         ))}
-        <div className="w-50 mx-auto my-3 bg-warning border rounded shadow-lg">
-          <span className="font-weight-bold d-block p-3">
-            {this.state.list.map(
-              (item, idx) =>
-                item.author ? (
-                  <div className="p-2 bg-info rounded border m-2 d-flex justify-content-around">
-                    <span>{idx}#</span>
-                    {item.author}
-                  </div>
-                ) : (
-                  ""
-                )
-            )}
-          </span>
-        </div>
       </div>
     );
   }
